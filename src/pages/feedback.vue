32172ed1e4eebbb42a25a40ba99723c317022a71
黄丽 <huangli@pisen.com.cn>
2018-10-15 15:10:02 +0800

    修复意见反馈上传图片不显示bug

--- src/pages/feedback.vue
+++ src/pages/feedback.vue
@@ -13,7 +13,7 @@
                 </span>
       </div>
       <div class="row">
-        <div class="item" v-for="item in imgs" v-lazy:background-image="item.url">
+        <div class="item" v-for="(item, index) in imgs" :key="index" v-lazy:background-image="filterImage(item.url)">
           <i class="del" @click="delImg(item.fileName)"></i>
         </div>
         <UploadFile @uploadCall="onRead" v-if="images.length < 8">
@@ -53,6 +53,15 @@
       document.title =this.$route.meta.title;
     },
     methods: {
+      filterImage (url) {
+        let index = url.indexOf("http")
+        if (index > 0) {
+          return url
+        } else {
+          let ip = this.api.ip.substring(0, this.api.ip.length - 1);
+          return ip + url
+        }
+      },
       onRead(image) {
         this.imgs.push(image);
       },
